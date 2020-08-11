f629a5f1c8e37a92715c4ddf17cf69feb300241c
MaleColomba <malenacolomba99@gmail.com>
2020-08-11 11:23:05 -0300

    Esno no es Amor

---
+++ js/app.js
@@ -0,0 +1,82 @@
+/*const seccionesPagina = new fullpage('#fullpage', {
+
+    autoScrolling: true,
+    fitToSection: false,
+    fitToSectionDeLay: 300,
+    easing: 'easeInOutCubic',
+    scrollingSpeed: 700,
+    css3: true,
+    easingcss3: 'ease-out',
+    loopBottom: false,
+});*/
+
+new fullpage('#fullpage', {
+	//Navegación
+	menu: '#menu',
+	lockAnchors: false,
+	anchors:['firstPage', 'secondPage'],
+	navigation: false,
+	navigationPosition: 'left',
+	navigationTooltips: ['firstSlide', 'secondSlide'],
+	showActiveTooltip: false,
+	slidesNavigation: false,
+	slidesNavPosition: 'bottom',
+
+	//Desplazamiento
+	css3: true,
+	scrollingSpeed: 0,
+	autoScrolling: true,
+	fitToSection: true,
+	fitToSectionDelay: 1000,
+	scrollBar: false,
+	easing: 'easeInOutCubic',
+	easingcss3: 'ease',
+	loopBottom: false,
+	loopTop: false,
+	loopHorizontal: true,
+	continuousVertical: false,
+	continuousHorizontal: false,
+	scrollHorizontally: false,
+	interlockedSlides: false,
+	dragAndMove: false,
+	offsetSections: false,
+	resetSliders: false,
+	fadingEffect: false,
+	normalScrollElements: '#element1, .element2',
+	scrollOverflow: false,
+	scrollOverflowReset: false,
+	scrollOverflowOptions: null,
+	touchSensitivity: 15,
+	bigSectionsDestination: null,
+
+	//Accesibilidad
+	keyboardScrolling: true,
+	animateAnchor: true,
+	recordHistory: true,
+
+	//Diseno
+	controlArrows: true,
+	verticalCentered: true,
+	fixedElements: '#header, .footer',
+	responsiveWidth: 0,
+	responsiveHeight: 0,
+	responsiveSlides: false,
+	parallax: false,
+	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+	cards: false,
+	cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
+
+	//Selectores personalizados
+	sectionSelector: '.section',
+	slideSelector: '.slide',
+
+	//Eventos
+	onLeave: function(origin, destination, direction){},
+	afterLoad: function(origin, destination, direction){},
+	afterRender: function(){},
+	afterResize: function(width, height){},
+	afterReBuild: function(){},
+	afterResponsive: function(isResponsive){},
+	afterSlideLoad: function(section, origin, destination, direction){},
+	onSlideLeave: function(section, origin, destination, direction){}
+});
